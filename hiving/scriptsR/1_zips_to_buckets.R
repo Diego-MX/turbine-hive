6a877071490530d162f42cc007b6aef4c2d6045d
Diego <dvillamil+gitkraken@gmail.com>
2020-03-26 18:41:15 -0600

    inicializar repo.

---
+++ hiving/scriptsR/1_zips_to_buckets.R
@@ -0,0 +1,6 @@
+# Diego Villamil, Turbine
+# CDMX, 26 de marzo de 2020
+
+# WindData, WindYucatan son zipfile que encontramos aparte. 
+# La extracción se hace manual porque está desordenada. 
+
---
+++ hiving/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Vamos a predecir el clima para que las compañías eólicas nos paguen. ',
+    author='Diegolas',
+    license='',
+)
